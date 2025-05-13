580c0fb93b46ca94404a1a74d25a69fe2574ee44
Quan Anh Nguyen <quannguy@c3a5c2.42berlin.de>
2025-05-13 14:24:13 +0200

    rename of file to more fitting name

---
+++ inc/config.hpp
@@ -0,0 +1,16 @@
+#ifndef CONFIG_HPP
+# define CONFIG_HPP
+
+#include <string>
+
+class config
+{
+	private:
+		std::string	listen;
+		int			port;
+		std::string	CGI_path
+		std::string	html_path;
+		error_path
+};
+
+#endif
